ffd4054b30cdc51bbc78fc339f2c648000f8b7b1
hamitmizrak <hamitmizrak@gmail.com>
2022-03-28 20:21:23 +0300

    css group selector

--- frontend/tutorials/_05_jquery/jquery.js
+++ frontend/tutorials/_05_jquery/jquery.js
@@ -9,15 +9,24 @@ DOM manipülasyonları yazma maliyeti daha düşüktür.
 */
 
 //Document.ready
-$(function (){
-    // JS DOM
-    //document.getElementById("jquery_id").innerHTML="<b>jquery buradaydı</b>";
+$(function () {
+  // JS DOM
+  //document.getElementById("jquery_id").innerHTML="<b>jquery buradaydı</b>";
 
-    // JQUERY
-    //HTML
-   $("#jquery_id").html("<b> <i>jquery buradaydı </i> </b>");
+  // JQUERY
+  //HTML
+  $('#jquery_id').html('<b> <i>jquery buradaydı </i> </b>')
+
+  //CSS
+  $('#jquery_id')
+    .css('background-color', 'red')
+    .css('color', 'blue')
+    .css('padding', '25px')
+
+  //pseudo element
+  $('p:contains(html)').css('box-shadow', '1px 1px 2px 5px rgba(2,2,2,.8');
 
 
-   //CSS
-   $("#jquery_id").css("background-color","red").css("color","blue").css("padding","25px");
+  $("ul li:not(':contains(css 3)')").css("color","blue").css("background","black");
+
 })
