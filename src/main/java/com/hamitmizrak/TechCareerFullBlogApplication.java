54dac26c7c0c73618e62895a9673f9b17c0b9817
hamitmizrak <hamitmizrak@gmail.com>
2022-03-28 20:00:11 +0300

    FrontEnd first commit

---
+++ src/main/java/com/hamitmizrak/TechCareerFullBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerFullBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCareerFullBlogApplication.class, args);
+    }
+
+}
