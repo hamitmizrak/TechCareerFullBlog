a73f57b37fbe3004f02ddcd1902996f256cf6df0
hamitmizrak <hamitmizrak@gmail.com>
2022-03-30 22:24:34 +0300

    Template Literal ,global scope, function scope ,let,const,var

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,7 +1,48 @@
 "use strict";
 
 //root
-var root = document.getElementById("root"); //  var variable1,variable2,variable3,variable4;
+var root = document.getElementById("root"); //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Hoisting: değişkeni sona yazıyoruz.
+
+deneme = "Merhabalar";
+var deneme; //global scope
+//  var globalScope="global değişken";
+//  let globalFunction=(p1)=>{
+// p1+=globalScope;
+// console.log(p1)
+//  }
+//local scope (FunctionScope)
+//  let functionScope=(p1)=>{
+// var localScope="global değişken";
+// p1+=localScope;
+// console.log(p1)
+//  }
+//  functionScope("Nasılsın");
+//  console.log(localScope)
+//++++ var , let ,const
+//VAR:aynı değişken isminden yazabiliriz
+//var
+//LET: aynı değişken isminden yazamayız
+//let
+//const
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Template Literal 
+
+var number1 = "Number1";
+var templateLiteralMultiLine = "template literal \nmulti-line";
+var templateLiteralInterpolation = "Ad\u0131m: ".concat(number1); //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//WWW=World Wide Web
+//ESMASCIPT:ECMA international tarafından standartlaştırılmış bir betik dilidir.
+//Js standart olması için kuruldu
+//TypeScript: Microsoft geliştiriyor , açık kaynaklı JavaScript'in bir üst kümesidir.
+//JavaScript'in tüm özelliklerini içerir
+//ts uzantıldır
+//ts hata denetimi vardır
+//Sınıf tabanlı objeler oluşturabiliriz.
+//interface,abstract,inheritance
+//TS data type: String, Number, Boolean,static,class Array, Enum, Tuple, generics'
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//  var variable1,variable2,variable3,variable4;
 //  variable1="Html5";
 //  variable2="Css3";
 //  variable3="JS";
@@ -28,6 +69,7 @@ var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.
   className: "list-group-item "
 }, object.variable3), /*#__PURE__*/React.createElement("li", {
   className: "list-group-item "
-}, object.variable4))); //ReactDOM
+}, object.variable4))); //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//ReactDOM
 
 ReactDOM.render(template, root);
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,63 @@
  
  //root
  var root = document.getElementById("root");
+
+ //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ //Hoisting: değişkeni sona yazıyoruz.
+ deneme="Merhabalar";
+ let deneme;
+
+ //global scope
+//  var globalScope="global değişken";
+//  let globalFunction=(p1)=>{
+// p1+=globalScope;
+// console.log(p1)
+//  }
+
+ //local scope (FunctionScope)
+//  let functionScope=(p1)=>{
+// var localScope="global değişken";
+// p1+=localScope;
+// console.log(p1)
+//  }
+//  functionScope("Nasılsın");
+//  console.log(localScope)
+//++++ var , let ,const
+//VAR:aynı değişken isminden yazabiliriz
+//var
+
+//LET: aynı değişken isminden yazamayız
+//let
+//const
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Template Literal 
+let number1="Number1";
+let templateLiteralMultiLine=`template literal 
+multi-line`;
+
+let templateLiteralInterpolation=`Adım: ${number1}`;
+
+
+ 
+ //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ //WWW=World Wide Web
+ //ESMASCIPT:ECMA international tarafından standartlaştırılmış bir betik dilidir.
+ //Js standart olması için kuruldu
+
+
+//TypeScript: Microsoft geliştiriyor , açık kaynaklı JavaScript'in bir üst kümesidir.
+//JavaScript'in tüm özelliklerini içerir
+//ts uzantıldır
+//ts hata denetimi vardır
+//Sınıf tabanlı objeler oluşturabiliriz.
+//interface,abstract,inheritance
+//TS data type: String, Number, Boolean,static,class Array, Enum, Tuple, generics'
+
+ //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
  
 //  var variable1,variable2,variable3,variable4;
 //  variable1="Html5";
@@ -8,7 +65,7 @@
 //  variable3="JS";
 //  variable4="React";
 var object={
- variable1:"Html5",
+variable1:"Html5",
 variable2:"Css3",
 variable3:"JS",
 variable4:"React",
@@ -33,6 +90,6 @@ variable4:"React",
  </div>
  );
  
-
+ //++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //ReactDOM
  ReactDOM.render(template, root);
