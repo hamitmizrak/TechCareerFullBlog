c9bbb5a8b28c69be4fcba69992a3f2a311b75722
hamitmizrak <hamitmizrak@gmail.com>
2022-03-30 19:35:50 +0300

    package.json

---
+++ frontend/Utils.js
@@ -0,0 +1,20 @@
+//Download: => https://nodejs.org/en/
+// node -v     ==> v16.14.2
+
+// npm  -v     ==> v8.5.0
+// npm update
+// npm list (local project)
+
+// npm bin -g  ==> npm yüklenecek dizin url (C:\usr\local)
+// npm list -g --depth 0 (npm yüklü olduğu dizindeki listelemek)
+// npm list -g --depth=0
+
+//npm init -y (package.json)
+
+
+//local packet
+// npm install <package>
+// npm i <package>
+// npm up <package> 
+// npm un <package> 
+
