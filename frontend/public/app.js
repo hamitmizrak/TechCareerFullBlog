fa1b0b476055d7b054bbf2b9932d60c7e7e6b010
hamitmizrak <hamitmizrak@gmail.com>
2022-03-30 21:42:22 +0300

    react jsx expression,object, commentline , className

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,11 +1,33 @@
 "use strict";
 
 //root
-var template = React.createElement("h3", {
-  id: "h3_id"
-}, "Babel var");
-var root = document.getElementById("root");
+var root = document.getElementById("root"); //  var variable1,variable2,variable3,variable4;
+//  variable1="Html5";
+//  variable2="Css3";
+//  variable3="JS";
+//  variable4="React";
+
+var object = {
+  variable1: "Html5",
+  variable2: "Css3",
+  variable3: "JS",
+  variable4: "React"
+}; //react yorumlama {}
+//react class => className
+//template
+
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Ba\u015Fl\u0131k"), /*#__PURE__*/React.createElement("p", {
+  className: "text-danger"
+}, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", {
+  className: "list-group"
+}, /*#__PURE__*/React.createElement("li", {
+  className: "list-group-item active"
+}, object.variable1), /*#__PURE__*/React.createElement("li", {
+  className: "list-group-item"
+}, object.variable2), /*#__PURE__*/React.createElement("li", {
+  className: "list-group-item "
+}, object.variable3), /*#__PURE__*/React.createElement("li", {
+  className: "list-group-item "
+}, object.variable4))); //ReactDOM
+
 ReactDOM.render(template, root);
-
-
-
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,38 @@
  
  //root
- 
- 
- var template = React.createElement("h3", {id:"h3_id"}, "Babel var");
-
  var root = document.getElementById("root");
+ 
+//  var variable1,variable2,variable3,variable4;
+//  variable1="Html5";
+//  variable2="Css3";
+//  variable3="JS";
+//  variable4="React";
+var object={
+ variable1:"Html5",
+variable2:"Css3",
+variable3:"JS",
+variable4:"React",
+}
+
+ //react yorumlama {}
+ //react class => className
+
+ //template
+ var template =
+ (
+<div>
+     <h1>Başlık</h1>
+     <p className="text-danger">Lorem ipsum dolor sit amet.</p>
+     <ul className="list-group">
+         {/* JSX Expression */}
+         <li className="list-group-item active">{object.variable1}</li>
+         <li className="list-group-item">{object.variable2}</li>
+         <li className="list-group-item ">{object.variable3}</li>
+         <li className="list-group-item ">{object.variable4}</li>
+     </ul>
+ </div>
+ );
+ 
+
+//ReactDOM
  ReactDOM.render(template, root);
