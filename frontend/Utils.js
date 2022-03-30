705d681d8b7e3367b169eba1fd7a90bdcae45efc
hamitmizrak <hamitmizrak@gmail.com>
2022-03-30 20:07:21 +0300

    React Libraries added

--- frontend/Utils.js
+++ frontend/Utils.js
@@ -1,6 +1,7 @@
 //Download: => https://nodejs.org/en/
 // node -v     ==> v16.14.2
 
+// NPM +++++++++++++++++++++++++++++++++++++++++++++++++++
 // npm  -v     ==> v8.5.0
 // npm update
 // npm list (local project)
@@ -12,9 +13,29 @@
 //npm init -y (package.json)
 
 
-//local packet
+// local packet
 // npm install <package>
 // npm i <package>
 // npm up <package> 
 // npm un <package> 
+
+//global packet (yani bilgisayarınızın her yerinden bu alana erişmek) ==> -g
+// npm install -g <package>
+
+////////////////////////////////////
+//live-server ++++++++++++++++++++++++
+// npm bin -g (C:\usr\local) 
+// npm list -g --depth=0
+// npm install -g live-server
+// npm install (package.json'deki dependency içeriğine göre tekrardan yükler)
+// npm config set prefix/usr/local (aman dikkat ==> bu kod npm setller )
+
+
+//////////////////////////////////
+// React içeriğinde Emmet özelliğini aktif etmek içindir.
+// Manage ==> setting.json
+// "emmet.includeLanguages": {
+//     "javascript": "javascriptreact"
+//   },
+
 
