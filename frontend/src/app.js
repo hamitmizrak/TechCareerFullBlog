1fbc059241fb7886b49b769ca28b6eff69b47bc6
hamitmizrak <hamitmizrak@gmail.com>
2022-04-01 21:48:02 +0300

    todoList

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,52 +1,50 @@
- // $("").
+// $("").
 
 //modal active
 $(function () {
-    $('#btn_giris').click(function () {
-      $('#myUye').modal('show')
-    })
-  
-    $('#deneme44').css('color', 'red').css('background', 'black');
-  
-    function onSubmitFunction(){
-      alert("submitted value 44");
+  $('#btn_giris').click(function () {
+    $('#myUye').modal('show')
+  })
+
+  $('#deneme44').css('color', 'red').css('background', 'black')
+
+  function onSubmitFunction() {
+    alert('submitted value 44')
+  }
+})
+
+//form validation
+$(function () {
+  //Submit  Tıklandığında
+  $('#btnSubmit').click(function () {
+    //input içeriği aldım
+    let email, password
+    email = $('#user_email').val()
+    password = $('#user_password').val()
+
+    //validation Email
+    if (email == '') {
+      $('#validation_email').html('email boş geçilemez....')
+    } else if (validateEmail(email) == false) {
+      $('#validation_email').html('email uygun formatta girmediniz....')
+    } else {
+      $('#validation_email').html('')
+    }
+    //validation Password
+    if (password == '') {
+      $('#validation_password').html('Şifre boş geçilemez....')
+    } else {
+      $('#validation_password').html('')
+    }
+
+    function validateEmail(email) {
+      let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return regex.test(email)
     }
   })
-  
-  //form validation
-  $(function () {
-    //Submit  Tıklandığında
-    $('#btnSubmit').click(function () {
-        
-      //input içeriği aldım
-      let email, password
-      email = $('#user_email').val()
-      password = $('#user_password').val()
-  
-      //validation Email
-      if (email == '') {
-        $('#validation_email').html('email boş geçilemez....')
-      } else if (validateEmail(email) == false) {
-        $('#validation_email').html('email uygun formatta girmediniz....')
-      }else{
-          $('#validation_email').html('')
-      }
-      //validation Password
-      if (password == '') {
-        $('#validation_password').html('Şifre boş geçilemez....')
-      }else{
-          $('#validation_password').html('')
-      }
-  
-      function validateEmail(email){
-          let regex=/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return regex.test(email);
-      }
-  
-    })
-  })
-  
- //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+})
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //  var root=document.getElementById("react_root");
 //  var template=(
 //    <div>
@@ -59,11 +57,10 @@ $(function () {
 
 //  ReactDOM.render(template,root);
 
-
 //    JS                      React
 //   ------                  ---------
 //    deneme                  {deneme}
-//    onclick="deneme()"      onClick={deneme} 
+//    onclick="deneme()"      onClick={deneme}
 
 //    Bootstrap               React
 //    ------                  ---------
@@ -73,48 +70,142 @@ $(function () {
 //    ------                  ---------
 //    style                   style={{fontSize:25px;}}
 
-
 //basket start
-var counter=0;
+var counter = 0
 //Artırmak
-let counterPlus=function(){
-  counter++;
-  againCall();
-  console.log("Artırıldı")
+let counterPlus = function () {
+  counter++
+  againCall()
+  console.log('Artırıldı')
 }
 
 //Azaltmak
-let counterMinus=function(){
-  counter--;
-  againCall();
-  console.log("Azaltıldı")
+let counterMinus = function () {
+  counter--
+  againCall()
+  console.log('Azaltıldı')
 }
 
 //Event
-function againCall(){
-  var eventTuto=(
+function againCall() {
+  var eventTuto = (
     <div>
       <div className="container">
         <div className="row">
-          <div className="col-md-8"><p>Adet: {counter}</p></div>
-          <div className="col-md-2"> <button onClick={counterPlus} className="btn btn-primary" style={{fontSize:"14px"}} id="plus_id"   >+</button></div>
-          <div className="col-md-2"> <button onClick={counterMinus} className="btn btn-danger" style={{fontSize:"14px"}} id="plus_id"   >-</button></div>
+          <div className="col-md-8">
+            <p>Adet: {counter}</p>
+          </div>
+          <div className="col-md-2">
+            {' '}
+            <button
+              onClick={counterPlus}
+              className="btn btn-primary"
+              style={{ fontSize: '14px' }}
+              id="plus_id"
+            >
+              +
+            </button>
+          </div>
+          <div className="col-md-2">
+            {' '}
+            <button
+              onClick={counterMinus}
+              className="btn btn-danger"
+              style={{ fontSize: '14px' }}
+              id="plus_id"
+            >
+              -
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  );
-  ReactDOM.render(eventTuto,document.getElementById("plus_minus"));
+  )
+  ReactDOM.render(eventTuto, document.getElementById('plus_minus'))
 }
-againCall();
- 
+againCall()
+
 //Timer
-function timerClock(){
-  var time=(
+function timerClock() {
+  var time = (
     <div>
-      <p><b>  {new Date().toLocaleTimeString()}  </b></p>
+      <p>
+        <b> {new Date().toLocaleTimeString()} </b>
+      </p>
     </div>
-  );
-  ReactDOM.render(time,document.getElementById("timer_id"));
+  )
+  ReactDOM.render(time, document.getElementById('timer_id'))
 }
 //bir metodu sürekli belirli aralıklarda çağırmak istersek
-setInterval(timerClock,1000);
+setInterval(timerClock, 1000)
+
+//TODO LIST
+//variable
+var listItemObject = {
+  title: 'Todo List Uygulaması',
+  listCounter: 'Liste Eleman sayısı',
+  data: [],
+}
+
+//LIST CLEAR
+let clearList=function(){
+  listItemObject.data=[];
+  formTutorial();
+}
+
+//onSubmit
+function formOnSubmit(event) {
+  //form bileşenin submit olmaması
+  event.preventDefault()
+  var formInputData = event.target.elements.formInput.value
+  //console.log(formInputData);
+  if (formInputData) {
+    listItemObject.data.push(formInputData)
+    formTutorial()
+    console.log('Eklendi: ' + formInputData)
+    event.target.elements.formInput.value='';
+  }
+}
+
+function formTutorial() {
+  var form = (
+    <div>
+      <h5 className="text-center44 text-uppercase mt-5">
+        {listItemObject.title}
+      </h5>
+      <p>
+        {listItemObject.listCounter} : {listItemObject.data.length}
+      </p>
+
+      {
+        <ul>
+          {listItemObject.data.map((temp) => {
+            return <li key={temp.toString()} > {temp} </li>
+          })}
+        </ul>
+      }
+
+      <form onSubmit={formOnSubmit}>
+        <div className="row">
+
+          <div className="col-md-6">
+            <input type="text" className="form-control" name="formInput" />
+          </div>
+
+          <div className="col-md-6">
+            <button className="btn btn-warning mr-3" style={{fontSize:".6rem"}} type="submit">
+              Gönder
+            </button>
+            <button onClick={clearList} className="btn btn-danger" style={{fontSize:".6rem"}}>
+              temizle
+            </button>
+          </div>
+         
+        </div>
+      </form>
+    </div>
+  )
+  ReactDOM.render(form, document.getElementById('todolistId'))
+}
+
+formTutorial()
--- frontend/template.js
+++ frontend/template.js
@@ -9,7 +9,7 @@ $(function () {
   $('#deneme44').css('color', 'red').css('background', 'black');
 
   function onSubmitFunction() {
-    alert("submitted value 44");
+    alert('submitted value 44');
   }
 }); //form validation
 
@@ -55,7 +55,7 @@ $(function () {
 //    JS                      React
 //   ------                  ---------
 //    deneme                  {deneme}
-//    onclick="deneme()"      onClick={deneme} 
+//    onclick="deneme()"      onClick={deneme}
 //    Bootstrap               React
 //    ------                  ---------
 //    class                   className
@@ -69,14 +69,14 @@ var counter = 0; //Artırmak
 var counterPlus = function counterPlus() {
   counter++;
   againCall();
-  console.log("Artırıldı");
+  console.log('Artırıldı');
 }; //Azaltmak
 
 
 var counterMinus = function counterMinus() {
   counter--;
   againCall();
-  console.log("Azaltıldı");
+  console.log('Azaltıldı');
 }; //Event
 
 
@@ -89,32 +89,95 @@ function againCall() {
     className: "col-md-8"
   }, /*#__PURE__*/React.createElement("p", null, "Adet: ", counter)), /*#__PURE__*/React.createElement("div", {
     className: "col-md-2"
-  }, " ", /*#__PURE__*/React.createElement("button", {
+  }, ' ', /*#__PURE__*/React.createElement("button", {
     onClick: counterPlus,
     className: "btn btn-primary",
     style: {
-      fontSize: "14px"
+      fontSize: '14px'
     },
     id: "plus_id"
   }, "+")), /*#__PURE__*/React.createElement("div", {
     className: "col-md-2"
-  }, " ", /*#__PURE__*/React.createElement("button", {
+  }, ' ', /*#__PURE__*/React.createElement("button", {
     onClick: counterMinus,
     className: "btn btn-danger",
     style: {
-      fontSize: "14px"
+      fontSize: '14px'
     },
     id: "plus_id"
   }, "-")))));
-  ReactDOM.render(eventTuto, document.getElementById("plus_minus"));
+  ReactDOM.render(eventTuto, document.getElementById('plus_minus'));
 }
 
 againCall(); //Timer
 
 function timerClock() {
-  var time = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, "  ", new Date().toLocaleTimeString(), "  ")));
-  ReactDOM.render(time, document.getElementById("timer_id"));
+  var time = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, " ", new Date().toLocaleTimeString(), " ")));
+  ReactDOM.render(time, document.getElementById('timer_id'));
 } //bir metodu sürekli belirli aralıklarda çağırmak istersek
 
 
-setInterval(timerClock, 1000);
+setInterval(timerClock, 1000); //TODO LIST
+//variable
+
+var listItemObject = {
+  title: 'Todo List Uygulaması',
+  listCounter: 'Liste Eleman sayısı',
+  data: []
+}; //LIST CLEAR
+
+var clearList = function clearList() {
+  listItemObject.data = [];
+  formTutorial();
+}; //onSubmit
+
+
+function formOnSubmit(event) {
+  //form bileşenin submit olmaması
+  event.preventDefault();
+  var formInputData = event.target.elements.formInput.value; //console.log(formInputData);
+
+  if (formInputData) {
+    listItemObject.data.push(formInputData);
+    formTutorial();
+    console.log('Eklendi: ' + formInputData);
+    event.target.elements.formInput.value = '';
+  }
+}
+
+function formTutorial() {
+  var form = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h5", {
+    className: "text-center44 text-uppercase mt-5"
+  }, listItemObject.title), /*#__PURE__*/React.createElement("p", null, listItemObject.listCounter, " : ", listItemObject.data.length), /*#__PURE__*/React.createElement("ul", null, listItemObject.data.map(function (temp) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: temp.toString()
+    }, " ", temp, " ");
+  })), /*#__PURE__*/React.createElement("form", {
+    onSubmit: formOnSubmit
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "row"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "col-md-6"
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    className: "form-control",
+    name: "formInput"
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "col-md-6"
+  }, /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-warning mr-3",
+    style: {
+      fontSize: ".6rem"
+    },
+    type: "submit"
+  }, "G\xF6nder"), /*#__PURE__*/React.createElement("button", {
+    onClick: clearList,
+    className: "btn btn-danger",
+    style: {
+      fontSize: ".6rem"
+    }
+  }, "temizle")))));
+  ReactDOM.render(form, document.getElementById('todolistId'));
+}
+
+formTutorial();
