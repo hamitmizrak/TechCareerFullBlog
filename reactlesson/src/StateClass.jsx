784bfdec988f42f8ede7108e032a708431754bd9
hamitmizrak <hamitmizrak@gmail.com>
2022-04-13 22:55:21 +0300

    revize

--- reactlesson/src/StateClass.jsx
+++ reactlesson/src/StateClass.jsx
@@ -6,10 +6,27 @@ constructor(props){
     //bind işlemi yapılmalı yoksa console.log(this) state bilgisine ulaşamamaz undefined olur
     this.fullInformation=this.fullInformation.bind(this);
     this.state = {
-        adi:"Hamit", 
-        soyadi:"Mızrak", 
-        dil:"Java Script"
+        adi:"default adi", 
+        soyadi:"default soyadi", 
+        dil:"default dil"
     }
+    
+}
+
+//compoenntDidMount: render sonrasında çalışmasını isteyeceğimiz  örneğin;servis çalışması için
+//1.constructor 2.render 3.componentDidMount
+componentDidMount(){
+  console.log("1.constructur 2.render 3.componentDidMount ")
+}
+
+//componentDidUpdate : state veya props bir değişiklik olduğunda çalışır
+componentDidUpdate(prevProps,prevState){
+  console.log("güncelleme geldi ")
+}
+
+//component silinirse 
+componentWillUnmount(){
+  console.log("Bilgi Component silinmiştir")
 }
 
 fullInformation(){
@@ -23,11 +40,28 @@ fullInformation(){
   console.log(this.state.adi)
 }
 
+objectToString(){
+  let obj={
+      adi:"Hamit",
+      soyadi:"Mızrak"
+  }
+
+  //objeyi string'e
+  const objectToString=JSON.stringify(obj);
+  console.log(objectToString)
+
+  //string'i object
+  const stringToObject=JSON.parse(objectToString);
+  console.log(stringToObject)
+}
+
+
   render() {
     return (
       <div>
           <p>{this.state.adi} - {this.state.soyadi} - {this.state.dil}</p>
          <button onClick={this.fullInformation}>information</button>
+         <button onClick={this.objectToString}>json</button>
       </div>
     )
   }
